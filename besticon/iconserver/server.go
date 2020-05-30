84946b0fa1df8b004dcc5684bd31f9311362d4c4
Taeil Kwak <taeil2@gmail.com>
2020-05-29 19:43:43 -0500

    edited CORS

--- besticon/iconserver/server.go
+++ besticon/iconserver/server.go
@@ -24,7 +24,18 @@ import (
 	_ "net/http/pprof"
 )
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, req)
+	if (*req).Method == "OPTIONS" {
+			return
+	}
+
 	if r.URL.Path == "" || r.URL.Path == "/" {
 		renderHTMLTemplate(w, 200, indexHTML, nil)
 	} else {
